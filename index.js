744e223b37a62cedbb85395f77802ae230b9e237
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-12 23:20:36 +0600

    bookings post method

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const USER_NAME =  process.env.DB_User;
-const USER_PASSWORD =  process.env.DB_password;
+const USER_NAME = process.env.DB_User;
+const USER_PASSWORD = process.env.DB_password;
 
 
 // /middleware 
 
 app.use(cors());
 app.use(express.json());
-console.log(process.env.DB_password , process.env.DB_User )
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -36,6 +36,38 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const serviceCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings');
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const options = {
+                projection: {  title: 1, price: 1 , service_id: 1 },
+
+            };
+            const result = await serviceCollection.findOne(query , options)
+
+
+            res.send(result)
+            console.log(result)
+
+        })
+ 
+        // Bookings
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body; 
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
